78eb296d76182da0ab118861dc5b27148e9a32c9
Andrew Lanin <ndrewln@gmail.com>
2015-06-30 00:08:09 +0300

    Basic layout

---
+++ data/exchanges.js
@@ -0,0 +1,17 @@
+function exchanges() {
+	return [	
+		{
+			"short_name": "NYSE",
+			"full_name": "New York Stock Exchange",
+			"trading_hours": [
+				{
+					"days": "Mon-Fri",
+					"type": "premarket",
+					"start": "08:30",
+					"end": "16:30"
+				}
+			],
+			"holidays": []
+		}
+	]
+}
