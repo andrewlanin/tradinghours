e8d51e6440ce4a1cc683ab96465b166eb1eb9fdf
alanin <alanin@nwork.local>
2015-08-14 17:22:14 +0300

    Current state (in session, premarket, etc.) indicator

--- site/js/main.js
+++ site/js/main.js
@@ -4,8 +4,9 @@ module.controller("ExchangeListController", [
                   "$scope", "$interval", "$data_engine",
                   function($scope, $interval, $data_engine) {
 	var render_current_time = function() {
-		$scope.now = moment.utc().format("HH:mm");
-		$scope.now_sec = moment.utc().format("ss");
+		$scope.now = moment.utc();
+		$scope.now_hm = $scope.now.format("HH:mm");
+		$scope.now_s = $scope.now.format("ss");
 	}
 
 	render_current_time();
@@ -13,7 +14,8 @@ module.controller("ExchangeListController", [
 	
 	$scope.exchanges = _.map(exchanges(), function(exchange){
 		exchange._tradingWeek = $data_engine.tradingWeek(exchange.trading_hours);
-		exchange._timeline = $data_engine.timeline(exchange._tradingWeek, moment.tz(exchange.timezone));
+		exchange._currentState = exchange._tradingWeek.frame($scope.now).type;
+		exchange._timeline = $data_engine.timeline(exchange._tradingWeek, $scope.now.tz(exchange.timezone));
 		return exchange;
 	});
 }])
